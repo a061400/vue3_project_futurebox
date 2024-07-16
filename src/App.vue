1e799dc1814e7e2129ca2976eb7c504131e97286
shawnlin <a061400@gmail.com>
2024-07-16 15:28:51 +0800

    新增一個Navbar檔案，並增加登出功能

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <button type="button" class="btn btn-primary">Primary</button>
-
+  <button type="button" class="btn btn-primary">App.vue</button>
   <router-view />
 </template>
 
