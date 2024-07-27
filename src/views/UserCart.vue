d7d84a13d0b769bd1b7fdec27c97620ea931ebaa
shawnlin <a061400@gmail.com>
2024-07-27 14:20:53 +0800

    新增用戶端的產品列表與相關頁面

---
+++ src/views/UserCart.vue
@@ -0,0 +1,78 @@
+<template>
+  <Loading :active="isLoading"></Loading>
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-md-7">
+        <table class="table align-middle">
+          <thead>
+          <tr>
+            <th>圖片</th>
+            <th>商品名稱</th>
+            <th>價格</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="item in products" :key="item.id">
+            <td style="width: 200px">
+              <div style="height: 100px; background-size: cover; background-position: center"
+                   :style="{backgroundImage: `url(${item.imageUrl})`}"></div>
+            </td>
+            <td><a href="#" class="text-dark">{{ item.title }}</a></td>
+            <td>
+              <div class="h5" v-if="!item.price">{{ item.origin_price }} 元</div>
+              <del class="h6" v-if="item.price">原價 {{ item.origin_price }} 元</del>
+              <div class="h5" v-if="item.price">現在只要 {{ item.price }} 元</div>
+            </td>
+            <td>
+              <div class="btn-group btn-group-sm">
+                <button type="button" class="btn btn-outline-secondary"
+                        @click="getProductDetail(item.id)">
+                  查看更多
+                </button>
+                <button type="button" class="btn btn-outline-danger"
+                        >
+                  加到購物車
+                </button>
+              </div>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- 購物車列表 -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: false,
+      products: {},
+    };
+  },
+  created() {
+    this.getProducts();
+  },
+  methods: {
+    getProducts(page = 1) {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products?page=${page}`;
+      this.isLoading = true;
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          console.log('客戶商品列表 取得成功', res.data);
+          this.products = res.data.products;
+        } else {
+          console.log('客戶商品列表 取得失敗');
+        }
+        this.isLoading = false;
+      });
+    },
+    getProductDetail(id) {
+      this.$router.push(`/user/product/${id}`);
+    },
+  },
+};
+</script>
---
+++ src/views/Userboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid mt-3 position-relative">
+    <h1 style="color: grey; background-color: lightyellow">
+      未來盒子 TheFutureBox 用戶端
+    </h1>
+  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/user/cart">取得產品列表</router-link>
+    </div>
+  </nav>
+  <div class="container-fluid mt-3 position-relative">
+    <ToastMessage></ToastMessage>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import ToastMessage from '@/components/ToastMessage.vue';
+import emitter from '@/methods/emitter';
+
+export default {
+  components: {
+    ToastMessage,
+  },
+
+  created() {},
+
+  // 讓所有內層元件皆可以使用
+  provide() {
+    return {
+      emitter,
+    };
+  },
+};
+</script>
