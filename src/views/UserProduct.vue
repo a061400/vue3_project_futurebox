85cc61e0023ba6067dba759fcb318c719a084c2f
shawnlin <a061400@gmail.com>
2024-07-30 11:29:24 +0800

    新增產品獨立頁面的加入購物車功能轉圈圈的顯示

--- src/views/UserProduct.vue
+++ src/views/UserProduct.vue
@@ -3,8 +3,12 @@
   <div class="container">
     <nav aria-label="breadcrumb">
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><router-link to="/user/cart">產品列表與購物車</router-link></li>
-        <li class="breadcrumb-item active" aria-current="page">{{ product.title }}</li>
+        <li class="breadcrumb-item">
+          <router-link to="/user/cart">產品列表與購物車</router-link>
+        </li>
+        <li class="breadcrumb-item active" aria-current="page">
+          {{ product.title }}
+        </li>
       </ol>
     </nav>
     <div class="row justify-content-center">
@@ -12,15 +16,32 @@
         <h2>{{ product.title }}</h2>
         <div>{{ product.content }}</div>
         <div>{{ product.description }}</div>
-        <img :src="product.imageUrl" alt="" class="img-fluid mb-3">
+        <img :src="product.imageUrl" alt="" class="img-fluid mb-3" />
       </article>
       <div class="col-4">
-        <div class="h5" v-if="!product.price">{{ product.origin_price }} 元</div>
-        <del class="h6" v-if="product.price">原價 {{ product.origin_price }} 元</del>
-        <div class="h5" v-if="product.price">現在只要 {{ product.price }} 元</div>
-        <hr>
-        <button type="button" class="btn btn-outline-danger"
-                @click="addToCart(product.id)">
+        <div class="h5" v-if="!product.price">
+          {{ product.origin_price }} 元
+        </div>
+        <del class="h6" v-if="product.price"
+          >原價 {{ product.origin_price }} 元</del
+        >
+        <div class="h5" v-if="product.price">
+          現在只要 {{ product.price }} 元
+        </div>
+        <hr />
+        <button
+          type="button"
+          class="btn btn-outline-danger"
+          @click="addToCart(product.id)"
+          :disabled="this.status.loadingItem === product.id"
+        >
+          <div
+            v-if="this.status.loadingItem === product.id"
+            class="spinner-border spinner-border-sm text-warning"
+            role="status"
+          >
+            <span class="visually-hidden">Loading...</span>
+          </div>
           加到購物車
         </button>
       </div>
